15712f826973dddf6dca24cac94d61c8b67972c7
Kanitkar, Umesh <Umesh.Kanitkar@neustar.biz>
2014-09-11 06:32:00 -0700

    Add logger

--- stubview/src/main/java/com/youmesh/App.java
+++ stubview/src/main/java/com/youmesh/App.java
@@ -1,13 +1,17 @@
 package com.youmesh;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private final static Logger LOG = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        LOG.info("test log");
     }
 }
